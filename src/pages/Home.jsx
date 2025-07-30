a53624ace247f777abdcebc23d9fbfeec144a498
Jacques Benzly Te <jacquezte@gmail.com>
2025-07-30 11:19:30 +0700

    Your commit message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -42,7 +42,7 @@ const Home = () => {
         <img
           src={process.env.PUBLIC_URL + "/Jacques.jpg"}
           alt="Jacques Te"
-          className="w-40 h-40 rounded-full shadow-md"
+          className="w-50 h-50 rounded-full shadow-md"
         />
       </div>
     </section>
